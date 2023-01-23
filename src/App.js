55b57256ef470467569bb5b8ff8893434556bcc3
Ayush Satvara <ayushsatvara3@gmail.com>
2023-01-23 14:44:53 +0530

    next commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Login from './components/login'
 import Signup from './components/signup'
 import Homepage from './components/homepage'
 import Mainpage from './components/mainpage'
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route exact path="/my-project-test/" element={<Homepage/>}/>
-      <Route exact path="/my-project-test/login" element={<Login/>}/>
-      <Route exact path="/my-project-test/signup" element={<Signup/>}/>
-      <Route exact path="/my-project-test/mainpage" element={<Mainpage/>}/>
-    </Routes>
+      <Routes basename="/my-project-test">
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/mainpage" element={<Mainpage />} />
+      </Routes>
     </>
   );
 }
